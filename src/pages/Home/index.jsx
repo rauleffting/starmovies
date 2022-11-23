94235e9543ee1e2285ebdc0751da0015722b6021
Raul Effting <rauleffting94@gmail.com>
2022-11-23 16:37:51 -0300

    Update index.jsx

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -17,7 +17,7 @@ export function Home() {
 
   useEffect(() => {
     async function fetchNotes(){
-      const response = await api.get(`/notes?title=${search}&tags=${search}`)
+      const response = await api.get(`/notes?title=${search}`)
       setNotes(response.data)
     }
 
